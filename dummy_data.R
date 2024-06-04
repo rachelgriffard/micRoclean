23a1e072dd84052f8190aabc4494eef0a61b4b78
Rachel Griffard <r816g589@kumc.edu>
2024-06-04 14:18:28 -0500

    Fixed dummy data
    Resolved naming issue, included dummy metadata and dummy technical replicates

--- dummy_data.R
+++ dummy_data.R
@@ -1,5 +1,8 @@
 ### dummy data for development
-dat = read.csv("Level6_datus.csv", header=T,row.name=1)
+dat = read.csv("Level6_Genus.csv", header=T,row.name=1)
+batch = dat$Batch
+group = dat$Groups
+
 index = grep(".*.g__*", colnames(dat)) # keep if have IDed datus
 dat = dat[,index]
 comp = data.frame(colnames(dat))
@@ -13,7 +16,32 @@ colnames(dat) = sub(".*.g__", "", colnames(dat)) # subset to only datus
 index2 = which(names(dat)=="")
 dat = dat[,-index2] # remove if no value
 
+## dummy res data for pipeline2
 res = read.csv('res_toydata.csv')
 
 ### dummy data for FL function
 removed = sample(rownames(FL), 100)
+
+### dummy meta data
+control = group
+control = control == "Negative Control"
+
+sample = group
+sample[!sample == "Control"] = "Plasma"
+
+dat = as.matrix(dat)
+
+meta = data.frame("is_control" = control,
+                  "sample" = sample,
+                  "batch" = batch)
+rownames(meta) = rownames(dat)
+
+### dummy technical replicates (p2s3)
+rs = data.frame("Batch1" = c("Old_trimmed_2", "Old_trimmed_86",
+                             "Old_trimmed_85", "Old_trimmed_49",
+                             "Old_trimmed_38", "Old_trimmed_3",
+                             "Old_trimmed_13", "Old_trimmed_26"),
+                "Batch2" = c("New_trimmed_29", "New_trimmed_35",
+                             "New_trimmed_41", "New_trimmed_47",
+                             "New_trimmed_53", "New_trimmed_59",
+                             "New_trimmed_65", "New_trimmed_71"))
