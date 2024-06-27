b252ba1f745f96d32b11ffc30b3b3fdad494afa4
Rachel Griffard <r816g589@kumc.edu>
2024-06-27 12:06:59 -0500

    Start package building

--- R/pipeline1.R
+++ R/pipeline1.R
@@ -3,8 +3,8 @@
 #' to closely characterize the composition of the data prior to contamination.
 #'
 #' @family pipeline1
-#' 
-#' @importFrom tidyverse %>%
+#'
+#' @import tidyverse
 #'
 #' @param counts Count matrix with samples as rows and features as counts
 #' @param meta Data frame with columns is_control, sample_type, and (optional) sample_well
@@ -15,37 +15,37 @@
 #' @exportClass list
 
 pipeline1 = function(counts, meta, control_order = NA, seed = 42) {
-  
+
   # check to ensure each batch contains some controls
   for(b in batch) {
     index = meta %>% filter(batch == b) %>% row.names() # select within batch
-    
+
     if (sum(meta[index, 'is_control']==TRUE) == 0) {
-      stop(paste0('To use pipeline1, all batches must contain controls. Batch named  ', 
+      stop(paste0('To use pipeline1, all batches must contain controls. Batch named  ',
                   b, ' does not contain samples specified as controls.', sep = '')) # break loop if missing
     }
   }
-  
+
   set.seed(seed)
-  
+
   batch = unique(meta$batch)
-  
+
   # SCRuB
   sc_outs = list() # get count matrices from batches scrubbed separately
   for(b in batch) {
     index = meta %>% filter(batch == b) %>% row.names() # select only of one batch
-    
+
     sc_outs[[b]] = SCRuB::SCRuB(counts[index,],
                                 meta[index,] %>%
                                   select(any_of(c('is_control', 'sample_type', 'sample_well'))))$decontaminated_samples
   }
-  
+
   sc_counts = do.call(rbind, sc_outs) # append batches back together
-  
+
   # Identify filtering loss
   sc_FL = FL(counts, new_counts = sc_counts)
-  
-  
+
+
   # Create deliverable
   return(list('decontaminated_count' = sc_counts,
               'filtering_loss' = sc_FL,
